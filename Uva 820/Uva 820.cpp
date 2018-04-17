7fa460939a29abaa7b442b79bc184af215528d0e
Kimbbakar <chowdhuryosman04@gmail.com>
2018-04-18 03:26:14 +0600

    Add a solution

---
+++ Uva 820/Uva 820.cpp
@@ -0,0 +1,254 @@
+ /*
+ Problem name :
+ Algorithm : Not Sure Yet
+ Contest/Practice :
+ Source :
+ Comment : Whenever you start to believe  yourself, people also start to believe in you
+ Date : --
+ Last Update : 25-Mar-2015
+*/
+
+#include<bits/stdc++.h>
+
+#define pause           system("pause");
+#define FOR(s,e,inc)    for(int i=s;i<=e;i+=inc)
+#define mod             1000000007
+#define UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())//vector must be sorted
+#define inf             1<<30
+#define pb              push_back
+#define ppb             pop_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define sz(x)           ((int)x.size())
+#define sqr(x)          ( (x)* (x) )
+#define eps             1e-9
+#define lcm(x,y)        (abs(x) /gcd(x,y))* abs(y)
+#define on(x,w)         x|(1<<w)
+#define check(x,w)      (x&(1<<w))
+#define all(x)          (x).begin(),(x).end()
+#define pf              printf
+#define sf              scanf
+#define pi              acos(-1.0)
+#define reset(x,v)      memset(x,v,sizeof(x));
+#define AND             &&
+#define OR              ||
+#define what_is(x)      cerr<<#x<<" is "<<x<<"\n";
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+using namespace std;
+
+
+template<class T>
+inline T mod_v(T num)
+{
+    if(num>=0)
+        return num%mod;
+    else
+        return (num%mod+mod)%mod;
+}
+template<class T>
+inline T gcd(T a,T b)
+{
+    a=abs(a);
+    b=abs(b);
+
+    while(b) b ^= a ^= b ^= a %= b;
+    return a;
+}
+
+template<class T>
+T fast_pow(T n , T p)
+{
+    if(p==0) return 1;
+    if(p%2)
+    {
+        T g=mod_v ( mod_v(n) * mod_v(fast_pow(n,p-1)) );
+        return g;
+    }
+    else
+    {
+        T g=fast_pow(n,p/2);
+        g=mod_v( mod_v(g) * mod_v(g) ) ;
+        return g;
+    }
+}
+
+template<class T>
+inline T modInverse(T n)
+{
+    return fast_pow(n,mod-2);
+}
+ 
+
+bool equalTo ( double a, double b ){ if ( fabs ( a - b ) <= eps ) return true; else return false; }
+bool notEqual ( double a, double b ){if ( fabs ( a - b ) > eps ) return true; else return false; }
+bool lessThan ( double a, double b ){ if ( a + eps < b ) return true; else return false; }
+bool lessThanEqual ( double a, double b ){if ( a < b + eps ) return true;   else return false;}
+bool greaterThan ( double a, double b ){if ( a > b + eps ) return true;else return false;}
+bool greaterThanEqual ( double a, double b ){if ( a + eps > b ) return true;else return false;}
+
+#define debug(args...) {dbg,args; cerr<<endl;}
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+int NextInt() { int n; scanf("%d", &n); return n; }
+long long NextLong() { long long n; scanf("%lld", &n); return n; }
+void print(int n){ printf("%d", n); }
+void println(int n){ printf("%d\n", n); }
+void println(long long n){ printf("%lld\n", n); }
+
+
+
+template<class T>
+inline int in(register T& num)
+{
+    cin>>num;
+    return 0;
+
+/*    register char c=0;
+    num=0;
+    bool n=false;
+    while(c<33)c=getchar();
+    while(c>33){
+        if(c=='-')
+            n=true;
+        else num=num*10+c-'0';
+        c=getchar();
+    }
+    num=n?-num:num;
+    return 1;*/
+}
+
+/******* ! Code start from here ! *******/
+
+
+/*
+max flow (dinitz algorithm)
+works on undirected graph
+can have loops, multiple edges, cycles
+*/
+
+#define MAXN 102
+#define MAXE 100002
+#define SET(x) reset(x,-1)
+
+
+int src, snk, nNode, nEdge;
+int Q[MAXN], fin[MAXN], pro[MAXN], dist[MAXN];
+int flow[MAXE], cap[MAXE], Next[MAXE], to[MAXE];
+
+inline void init(int _src, int _snk, int _n) {
+    src = _src, snk = _snk, nNode = _n, nEdge = 0;
+    SET(fin);
+}
+
+inline void add(int u, int v, int _cap) {
+    to[nEdge] = v, cap[nEdge] = _cap, flow[nEdge] = 0;
+    Next[nEdge] = fin[u], fin[u] = nEdge++;
+    to[nEdge] = u, cap[nEdge] = _cap, flow[nEdge] = 0;
+    Next[nEdge] = fin[v], fin[v] = nEdge++;
+}
+
+bool bfs() {
+    int st, en, i, u, v;
+    SET(dist);
+    dist[src] = st = en = 0;
+    Q[en++] = src;
+    while(st < en) {
+        u = Q[st++];
+        for(i=fin[u]; i>=0; i=Next[i]) {
+            v = to[i];
+            if(flow[i] < cap[i] && dist[v]==-1) {
+                dist[v] = dist[u]+1;
+                Q[en++] = v;
+            }
+        }
+    }
+    return dist[snk]!=-1;
+}
+
+int dfs(int u, int fl) {
+    if(u==snk) return fl;
+    for(int &e=pro[u], v, df; e>=0; e=Next[e]) {
+        v = to[e];
+        if(flow[e] < cap[e] && dist[v]==dist[u]+1) {
+            df = dfs(v, min(cap[e]-flow[e], fl));
+            if(df>0) {
+                flow[e] += df;
+                flow[e^1] -= df;
+                return df;
+            }
+        }
+    }
+    return 0;
+}
+
+#define i64 int
+#define INF inf
+
+i64 dinitz() {
+    i64 ret = 0;
+    int df;
+    while(bfs()) {
+        for(int i=1; i<=nNode; i++) pro[i] = fin[i];
+        while(true) {
+            df = dfs(src, INF);
+            if(df) ret += (i64)df;
+            else break;
+        }
+    }
+    return ret;
+}
+
+ 
+int main()
+{
+     std::ios_base::sync_with_stdio(false);
+
+    #ifdef kimbbakar
+        freopen ( "E:/Code/in.txt", "r", stdin );
+//        freopen ( "E:/Code/out.txt", "w", stdout );
+    #endif
+
+
+    int tcase=1;
+
+     
+    int n ,s,t,c,e;
+
+    while(1){
+        in(n) ;
+
+        if(!n) break;
+
+        in(s),in(t),in(e);
+
+        init(s,t, n);
+
+        while(e--){
+            in(s),in(t),in(c);
+
+            add(s,t,c);
+        }
+ 
+
+
+        pf("Network %d\nThe bandwidth is %d.\n\n",tcase++,dinitz() );
+
+    }
+
+
+
+    return 0;
+}
+
+
+
