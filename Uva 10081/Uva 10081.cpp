27fd795ac2266286c1e9618f39623b4b02c8983d
Kimbbakar <chowdhuryosman04@gmail.com>
2018-06-09 05:43:34 +0600

    Add a solution

---
+++ Uva 10081/Uva 10081.cpp
@@ -0,0 +1,170 @@
+#include<bits/stdc++.h>
+
+#define pause           system("pause");
+#define FOR(s,e,inc)    for(int i=s;i<=e;i+=inc)
+#define mod             1000000007
+#define inf             1<<30
+#define pb              push_back
+#define ppb             pop_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define sz(x)           ((int)x.size())
+#define sqr(x)          ( (x)* (x) )
+#define eps             1e-9
+#define lcm(x,y)        (abs(x) /gcd(x,y))* abs(y)
+#define on(x,w)         x|(1<<w)
+#define check(x,w)      (x&(1<<w))
+#define all(x)          (x).begin(),(x).end()
+#define pf              printf
+#define sf              scanf
+#define pi              acos(-1.0)
+#define reset(x,v)      memset(x,v,sizeof(x));
+#define AND             &&
+#define OR              ||
+#define what_is(x)      cerr<<#x<<" is "<<x<<"\n";
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+using namespace std;
+
+
+template<class T>
+inline T mod_v(T num)
+{
+    if(num>=0)
+        return num%mod;
+    else
+        return (num%mod+mod)%mod;
+}
+template<class T>
+inline T gcd(T a,T b)
+{
+    a=abs(a);
+    b=abs(b);
+
+    while(b) b ^= a ^= b ^= a %= b;
+    return a;
+}
+
+template<class T>
+T fast_pow(T n , T p)
+{
+    if(p==0) return 1;
+    if(p%2)
+    {
+        T g=mod_v ( mod_v(n) * mod_v(fast_pow(n,p-1)) );
+        return g;
+    }
+    else
+    {
+        T g=fast_pow(n,p/2);
+        g=mod_v( mod_v(g) * mod_v(g) ) ;
+        return g;
+    }
+}
+
+template<class T>
+inline T modInverse(T n)
+{
+    return fast_pow(n,mod-2);
+}
+
+bool equalTo ( double a, double b ){ if ( fabs ( a - b ) <= eps ) return true; else return false; }
+bool notEqual ( double a, double b ){if ( fabs ( a - b ) > eps ) return true; else return false; }
+bool lessThan ( double a, double b ){ if ( a + eps < b ) return true; else return false; }
+bool lessThanEqual ( double a, double b ){if ( a < b + eps ) return true;   else return false;}
+bool greaterThan ( double a, double b ){if ( a > b + eps ) return true;else return false;}
+bool greaterThanEqual ( double a, double b ){if ( a + eps > b ) return true;else return false;}
+
+#define debug(args...) {dbg,args; cerr<<endl;}
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+int nextInt() { int n; scanf("%d", &n); return n; }
+long long nextLong() { long long n; scanf("%lld", &n); return n; }
+void print(int n){ printf("%d", n); }
+void println(int n){ printf("%d\n", n); }
+void println(long long n){ printf("%lld\n", n); }
+
+
+
+template<class T>
+inline int in(register T& num)
+{
+    register char c=0;
+    num=0;
+    bool n=false;
+    while(c<33)c=getchar();
+    while(c>33){
+        if(c=='-')
+            n=true;
+        else num=num*10+c-'0';
+        c=getchar();
+    }
+    num=n?-num:num;
+    return 1;
+}
+
+/******* ! Code start from here ! *******/
+
+double dp[102][12];
+int done[102][12],cc=1;
+double val ;
+int n,k;
+
+double re(int p,int nw){
+
+    if(p==n-1)
+        return val;
+
+    double &res = dp[p][nw];
+
+    if(done[p][nw]==cc) return res;
+
+    done[p][nw]= cc;
+    res = re(p+1,nw) ;
+    if(nw+1<=k)
+        res+=re(p+1,nw+1);
+    if(nw-1>=0)
+        res+=re(p+1,nw-1);
+
+    return res;
+
+}
+
+
+int main()
+{
+//     std::ios_base::sync_with_stdio(false);
+
+    #ifdef kimbbakar
+        freopen ( "in.txt", "r", stdin );
+//        freopen ( "out.txt", "w", stdout );
+    #endif
+
+
+    while(scanf("%d %d",&k,&n)==2){
+        val = 100;
+
+        for(int i=1;i<=n;i++)
+            val/=double(k+1);
+
+
+        double res = 0;
+
+        for(int i=0;i<=k;i++)
+            res+=re(0,i);
+        pf("%.5f\n", res);
+
+        cc++;
+    }
+
+    return 0;
+}
---
+++ Uva 1099/Uva 1099.cpp
@@ -0,0 +1,202 @@
+ /*
+ Problem name :
+ Algorithm : Not Sure Yet
+ Contest/Practice :
+ Source :
+ Comment : Whenever you start to believe  yourself, people also start to believe in you
+ Date : --
+ Last Update : 25-Mar-2015
+*/
+
+#include<bits/stdc++.h>
+
+#define pause           system("pause");
+#define FOR(s,e,inc)    for(int i=s;i<=e;i+=inc)
+#define mod             1000000007
+#define UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())//vector must be sorted
+#define inf             1<<30
+#define pb              push_back
+#define ppb             pop_back
+#define mp              make_pair
+#define F               first
+#define S               second
+#define sz(x)           ((int)x.size())
+#define sqr(x)          ( (x)* (x) )
+#define eps             1e-9
+#define lcm(x,y)        (abs(x) /gcd(x,y))* abs(y)
+#define on(x,w)         x|(1<<w)
+#define check(x,w)      (x&(1<<w))
+#define all(x)          (x).begin(),(x).end()
+#define pf              printf
+#define sf              scanf
+#define pi              acos(-1.0)
+#define reset(x,v)      memset(x,v,sizeof(x));
+#define AND             &&
+#define OR              ||
+#define what_is(x)      cerr<<#x<<" is "<<x<<"\n";
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+using namespace std;
+
+
+template<class T>
+inline T mod_v(T num)
+{
+    if(num>=0)
+        return num%mod;
+    else
+        return (num%mod+mod)%mod;
+}
+template<class T>
+inline T gcd(T a,T b)
+{
+    a=abs(a);
+    b=abs(b);
+
+    while(b) b ^= a ^= b ^= a %= b;
+    return a;
+}
+
+template<class T>
+T fast_pow(T n , T p)
+{
+    if(p==0) return 1;
+    if(p%2)
+    {
+        T g=mod_v ( mod_v(n) * mod_v(fast_pow(n,p-1)) );
+        return g;
+    }
+    else
+    {
+        T g=fast_pow(n,p/2);
+        g=mod_v( mod_v(g) * mod_v(g) ) ;
+        return g;
+    }
+}
+
+template<class T>
+inline T modInverse(T n)
+{
+    return fast_pow(n,mod-2);
+}
+ 
+
+bool equalTo ( double a, double b ){ if ( fabs ( a - b ) <= eps ) return true; else return false; }
+bool notEqual ( double a, double b ){if ( fabs ( a - b ) > eps ) return true; else return false; }
+bool lessThan ( double a, double b ){ if ( a + eps < b ) return true; else return false; }
+bool lessThanEqual ( double a, double b ){if ( a < b + eps ) return true;   else return false;}
+bool greaterThan ( double a, double b ){if ( a > b + eps ) return true;else return false;}
+bool greaterThanEqual ( double a, double b ){if ( a + eps > b ) return true;else return false;}
+
+#define debug(args...) {dbg,args; cerr<<endl;}
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+int nextInt() { int n; scanf("%d", &n); return n; }
+long long nextLong() { long long n; scanf("%lld", &n); return n; }
+void print(int n){ printf("%d", n); }
+void println(int n){ printf("%d\n", n); }
+void println(long long n){ printf("%lld\n", n); }
+
+
+
+template<class T>
+inline int in(register T& num)
+{
+    register char c=0;
+    num=0;
+    bool n=false;
+    while(c<33)c=getchar();
+    while(c>33){
+        if(c=='-')
+            n=true;
+        else num=num*10+c-'0';
+        c=getchar();
+    }
+    num=n?-num:num;
+    return 1;
+}
+
+/******* ! Code start from here ! *******/
+
+int n;
+int kp[20];
+int l,r;
+int sum[1<<15];
+int GCD[1<<15];
+bool dp[1<<15][102];
+int done[1<<15][102],cc=1;
+
+bool dp(int mask,int lenght){
+
+    if(mask==0) return (lenght==0); 
+
+    bool &res = dp[mask][lenght];
+
+    if(done[mask][lenght]==cc) return res;
+    done[mask][lenght]= cc;
+
+    res = false;
+    int weight = (l*r -sum[mask])/lenght;
+
+    int unmark = ((1<<n) -1 )^mask;
+
+    for(int i=unmark;i>0;i = (i-1)&unmark){
+        
+
+
+    }
+
+    return res;
+
+}
+
+
+int main()
+{
+//     std::ios_base::sync_with_stdio(false);
+
+    #ifdef kimbbakar
+        freopen ( "E:/Code/in.txt", "r", stdin );
+//        freopen ( "E:/Code/out.txt", "w", stdout );
+    #endif
+
+    while(1){ 
+        in(n);
+
+        if(!n) break;
+        in(l),in(r);
+
+        for(int i=0;i<n;i++)
+            in(kp[i]);
+
+        for(int i=0;i<(1<<n);i++ ){
+            sum[i] = 0;
+            GCD[i] = 0;
+
+            for(int j=0;j<n;j++){
+                if((i&(1<<j) ) ){
+                    sum[i]+=kp[j];                    
+                    GCD[i]=gcd(GCD[i],kp[j] ) ;                    
+                }
+
+            }
+        }
+
+
+
+    }
+
+
+    return 0;
+}
+
+
+
